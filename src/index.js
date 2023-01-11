31537972e266ba12ba85c742989c736f2f560755
Yuliya Kotova <yulya9449@yandex.ru>
2023-01-11 18:25:01 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
+  if (n === Math.abs(n)){
+    return reverseForPlus(n)
+    };
+  {return reverseForPlus(-n)
+    };
+  };
   
-}
+  function reverseForPlus(n) {
+    let sum = 0;
+    for ( let i = n; i > 0; i = Math.trunc(i / 10)) {
+      let remainder = i % 10;
+      sum = sum * 10 + remainder;
+      };
+    return sum;
+    };
+
+    // console.log(reverse(-190));
